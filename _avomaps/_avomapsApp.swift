b125deb350c6662ecebb6b310b91ca473226f6bd
ethanherrera <ethan.herrera.engineer@gmail.com>
2023-03-20 15:27:12 -0700

    Initial Commit

---
+++ _avomaps/_avomapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _avomapsApp.swift
+//  _avomaps
+//
+//  Created by Ethan Herrera on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct _avomapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
